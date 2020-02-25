e9abe41d7df266a9928c9f863483a56b9d0a233c
gitMarcldo <marcldo@users.noreply.github.com>
2020-02-24 21:35:00 -0500

    CAPITALIZE LETTERS

--- Reversals_FizzBuzz_MaxChar/index.js
+++ Reversals_FizzBuzz_MaxChar/index.js
@@ -37,8 +37,7 @@ function isPalindrome(str) {
 
 }
 
-// Challenge 3: reverse and integer
-
+// Challenge 3: reverse an integer
 function reverseInt(int) {
 
   const strInt = int.toString();
@@ -52,6 +51,41 @@ function reverseInt(int) {
 
 }
 
-const output = reverseInt(123456);
+// CHALLENGE 4: CAPITALIZE LETTERS
+// Return a  string with the first letter of every word capitalized
+// ex. capitalizeLetters('i love javascript') === 'I Love Javascript'
+function capitalizeLetters(str) {
+
+  //first letter is uppercase
+  let capStr = str[0].toUpperCase()
+
+  //loop at index 1 to account for first letter
+  for (let i = 1; i < str.length; i++) {
+    if (str[i - 1] === ' ') {
+      capStr = capStr + str[i].toUpperCase()
+    } else {
+      capStr = capStr + str[i]
+    }
+  }
+
+  return capStr
+}
+
+
+// CHALLENGE 5: MAX CHARACTER
+// Return a the most common character in a string
+// ex. maxCharacter('javascript') == 'a'
+function maxCharacter(str) {
+
+}
+
+// CHALLENGE 6: FIZZBUZZ
+// prints all numbers from 1 to 100. For multiples of 3 print "Fizz"
+// for multiples of 5 print "Buzz". For numbers which are both 3 and 5, print "FizzBuzz"
+function fizzBuzz() {
+
+}
+
+const output = capitalizeLetters("i love javascript");
 
 console.log(output)
